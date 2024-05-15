4fdfa089839bad768bc7c85e6d258f00cfa2c4b9
ShivrajPhadtare <sjp2032001@gmail.com>
2024-05-15 23:53:07 +0530

    new

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+load_dotenv() # load the .env file
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input_prompt,image):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input_prompt,image[0]])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type":uploaded_file.type, # Get the mime type of the uploaded image
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
+## initialize our streamlit app FrontEnd
+
+st.set_page_config(page_title="Calories Advisor App")
+
+st.header("Calories Advisor App")
+uploaded_file = st.file_uploader("Choose an image..",type=["jpg","jpeg","png"])
+image=""
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+submit=st.button("Tell me about the total calories")
+
+## AI prompt
+
+input_prompt = """
+You are an expert in nutritionist where you need to see the food items from the image
+               and calculate the total calories, also provide the details of every food items with calories intake
+               is below format
+
+               1. Item 1 - no of calories
+               2. Item 2 - no of calories
+               ----
+               ----
+        Finally you can mention whether the food is healthy or not and also mention the percentage split of the ratio of carbohydrates, fats,protin , fibers, suger and other important thingsrequired in our diet
+"""
+
+if submit:
+    image_data= input_image_setup(uploaded_file)
+    response= get_gemini_response(input_prompt,image_data)
+    st.header("The Response is")
+    st.write(response)
+
+
+
